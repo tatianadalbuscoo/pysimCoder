70d022d4e4481407dfb5cb36cbb878ae04675001
tatiana.dalbusco <tatiana.dalbusco@student.supsi.ch>
2024-11-02 01:02:49 +0100

    Fix GPIO

--- CodeGen/Delfino/src/button.c
+++ CodeGen/Delfino/src/button.c
@@ -7,33 +7,39 @@ void Button_Init(int pin)
     GPIO_SetupPinOptions(pin, GPIO_INPUT, GPIO_PULLUP);
 }
 
+/*
+* e.g.: if pin = 34
+* pin - 32 = 2
+* 1 << 2 = 00000100
+*/
 Uint16 Button_IsPressed(int pin) {
+
+    // GPIO 0-31 -> GPADAT
     if (pin >= 0 && pin <= 31) {
-        // GPIO 0-31 -> GPADAT
         return (GpioDataRegs.GPADAT.all & (1 << pin)) == 0;
     }
+    // GPIO 32-63 -> GPBDAT
     else if (pin >= 32 && pin <= 63) {
-        // GPIO 32-63 -> GPBDAT
         return (GpioDataRegs.GPBDAT.all & (1 << (pin - 32))) == 0;
     }
+    // GPIO 64-95 -> GPCDAT
     else if (pin >= 64 && pin <= 95) {
-        // GPIO 64-95 -> GPCDAT
         return (GpioDataRegs.GPCDAT.all & (1 << (pin - 64))) == 0;
     }
+    // GPIO 96-127 -> GPDDAT
     else if (pin >= 96 && pin <= 127) {
-        // GPIO 96-127 -> GPDDAT
         return (GpioDataRegs.GPDDAT.all & (1 << (pin - 96))) == 0;
     }
+    // GPIO 128-159 -> GPEDAT
     else if (pin >= 128 && pin <= 159) {
-        // GPIO 128-159 -> GPEDAT
         return (GpioDataRegs.GPEDAT.all & (1 << (pin - 128))) == 0;
     }
+    // GPIO 160-168 -> GPFDAT
     else if (pin >= 160 && pin <= 168) {
-        // GPIO 160-168 -> GPFDAT
         return (GpioDataRegs.GPFDAT.all & (1 << (pin - 160))) == 0;
     }
+    // Invalid pin
     else {
-        // Invalid pin, return 0 or an error code as needed
         return 0;
     }
 }
--- CodeGen/Delfino/src/led.c
+++ CodeGen/Delfino/src/led.c
@@ -7,47 +7,59 @@ void LED_Init(int pin)
     GPIO_SetupPinOptions(pin, GPIO_OUTPUT, GPIO_PUSHPULL);
 }
 
+/*
+* e.g.: if pin = 34
+* pin - 32 = 2
+* 1 << 2 = 00000100
+*/
 void LED_On(int pin) {
-    // Turn on the LED on the specified pin (active-low LED)
+
+    // Turn on the LED on the specified pin
     if (pin >= 0 && pin < 32) {
-        GpioDataRegs.GPACLEAR.all = (1 << pin); // For pins 0-31
+        GpioDataRegs.GPACLEAR.all = (1 << pin); 
     }
     else if (pin >= 32 && pin < 64) {
-        GpioDataRegs.GPBCLEAR.all = (1 << (pin - 32)); // For pins 32-63
+        GpioDataRegs.GPBCLEAR.all = (1 << (pin - 32)); 
     }
     else if (pin >= 64 && pin < 96) {
-        GpioDataRegs.GPCCLEAR.all = (1 << (pin - 64)); // For pins 64-95
+        GpioDataRegs.GPCCLEAR.all = (1 << (pin - 64)); 
     }
     else if (pin >= 96 && pin < 128) {
-        GpioDataRegs.GPDCLEAR.all = (1 << (pin - 96)); // For pins 96-127
+        GpioDataRegs.GPDCLEAR.all = (1 << (pin - 96)); 
     }
     else if (pin >= 128 && pin < 160) {
-        GpioDataRegs.GPECLEAR.all = (1 << (pin - 128)); // For pins 128-159
+        GpioDataRegs.GPECLEAR.all = (1 << (pin - 128)); 
     }
     else if (pin >= 160 && pin < 169) {
-        GpioDataRegs.GPFCLEAR.all = (1 << (pin - 160)); // For pins 160-168
+        GpioDataRegs.GPFCLEAR.all = (1 << (pin - 160));
     }
 }
 
+/*
+* eg: if pin = 34
+* pin - 32 = 2
+* 1 << 2 = 00000100
+*/
 void LED_Off(int pin) {
-    // Turn off the LED on the specified pin (active-low LED)
+
+    // Turn off the LED on the specified pin
     if (pin >= 0 && pin < 32) {
-        GpioDataRegs.GPASET.all = (1 << pin); // For pins 0-31
+        GpioDataRegs.GPASET.all = (1 << pin);
     }
     else if (pin >= 32 && pin < 64) {
-        GpioDataRegs.GPBSET.all = (1 << (pin - 32)); // For pins 32-63
+        GpioDataRegs.GPBSET.all = (1 << (pin - 32));
     }
     else if (pin >= 64 && pin < 96) {
-        GpioDataRegs.GPCSET.all = (1 << (pin - 64)); // For pins 64-95
+        GpioDataRegs.GPCSET.all = (1 << (pin - 64));
     }
     else if (pin >= 96 && pin < 128) {
-        GpioDataRegs.GPDSET.all = (1 << (pin - 96)); // For pins 96-127
+        GpioDataRegs.GPDSET.all = (1 << (pin - 96));
     }
     else if (pin >= 128 && pin < 160) {
-        GpioDataRegs.GPESET.all = (1 << (pin - 128)); // For pins 128-159
+        GpioDataRegs.GPESET.all = (1 << (pin - 128));
     }
     else if (pin >= 160 && pin < 169) {
-        GpioDataRegs.GPFSET.all = (1 << (pin - 160)); // For pins 160-168
+        GpioDataRegs.GPFSET.all = (1 << (pin - 160));
     }
 }
 
